a22e587406679a96c759170b400bc03074b13b57
starzzra <daneerameijandiniariestya@gmail.com>
2025-05-27 08:08:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,11 @@ st.title("🩷 Greetings! welcome to my very first app <3")
 st.write(
     "Because this was my first app, Let me introduce myself first so you can know me better! Im Daneera Meijandini Ariestya from X-F :D"
 )
+
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil") 
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1) 
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah Bilangan Genap") 
+else:
+    st.write(f"{angka} adalah Bilangan Ganjil") 
